f59af45b5f7c3a3e9cd9ddf235ada5f80cf94560
Sithum Bopitiya <sithumsandeepap@gmail.com>
2022-04-02 14:19:17 +0530

    Rm warn

--- src/main/java/com/example/resttwo/repository/DBConnection.java
+++ src/main/java/com/example/resttwo/repository/DBConnection.java
@@ -5,18 +5,20 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-  public Connection getConnection() {
-    Connection con = null;
-    try {
-      Class.forName("com.mysql.jdbc.Driver");
-      String url = System.getenv("JDBC_DATABASE_URL") != null ? System.getenv("JDBC_DATABASE_URL") : "jdbc:mysql://localhost:3306/productDB";
-      String user = System.getenv("JDBC_DATABASE_USERNAME") != null ? System.getenv("JDBC_DATABASE_USERNAME") : "root";
-      String password = System.getenv("JDBC_DATABASE_PASSWORD") != null ? System.getenv("JDBC_DATABASE_PASSWORD") : "mysespw";
-      con = DriverManager.getConnection(url, user, password);
-    } catch (Exception e) {
-      e.printStackTrace();
+    final String URL = System.getenv("JDBC_DATABASE_URL") != null ? System.getenv("JDBC_DATABASE_URL") : "jdbc:mysql://localhost:3306/productDB";
+    final String USER = System.getenv("JDBC_DATABASE_USERNAME") != null ? System.getenv("JDBC_DATABASE_USERNAME") : "root";
+    final String PASSWORD = System.getenv("JDBC_DATABASE_PASSWORD") != null ? System.getenv("JDBC_DATABASE_PASSWORD") : "mysespw";
+
+    public Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
     }
-    return con;
-  }
 
 }
