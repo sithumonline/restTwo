62ca344e6dcbf2bfc8c0b22337a68c81a9c4cba2
Sithum Bopitiya <sithumsandeepap@gmail.com>
2022-04-02 15:55:18 +0530

    Bff for category

--- src/main/java/com/example/resttwo/controller/CategoryController.java
+++ src/main/java/com/example/resttwo/controller/CategoryController.java
@@ -1,34 +1,37 @@
 package com.example.resttwo.controller;
 
-import java.util.HashMap;
 import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
-import com.example.resttwo.model.Category;
-import com.example.resttwo.service.CategoryService;
+import java.util.HashMap;
 
 /*
- *default Port : 3080
- *http://localhost:3080/api/v1/category/*
+ *default Port : 4080
+ *http://localhost:4080/api/v1/category/*
 */
 @Path("/api/v1/category")
 public class CategoryController {
-	private Category category;
-	private final CategoryService categoryService = new CategoryService();
+	private final Client client;
+	private final String BASE_URI;
+
+	public CategoryController(String baseUri) {
+		this.client = ClientBuilder.newClient();
+		this.BASE_URI = baseUri + "/category/";
+	}
 
 	@POST
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response addCategory(HashMap<String, ?> categoryData) {
-		String name = (String) categoryData.get("name");
-		String description = (String) categoryData.get("description");
-		System.out.println(
-				"CategoryController.addCategory() : name = " + name + " description = " + description);
-		category = new Category(name, description);
-
-		return categoryService.addCategory(category);
+		Entity<HashMap<String, ?>> entity = Entity.entity(categoryData, MediaType.APPLICATION_JSON);
+		return client.target(BASE_URI)
+				.request(MediaType.APPLICATION_JSON)
+				.post(entity, Response.class);
 	}
 
 	@PUT
@@ -36,19 +39,19 @@ public class CategoryController {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response updateCategory(HashMap<String, ?> categoryData, @PathParam("categoryId") Integer categoryId) {
-		String name = (String) categoryData.get("name");
-		String description = (String) categoryData.get("description");
-		category = new Category(name, description);
-		category.setId(categoryId);
-
-		return categoryService.updateCategory(category);
+		Entity<HashMap<String, ?>> entity = Entity.entity(categoryData, MediaType.APPLICATION_JSON);
+		return client.target(BASE_URI + categoryId)
+				.request(MediaType.APPLICATION_JSON)
+				.put(entity, Response.class);
 	}
 
 	@GET
 	@Path("/")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getCategorys() {
-		return categoryService.getCategorys();
+		return client.target(BASE_URI)
+				.request(MediaType.APPLICATION_JSON)
+				.get(Response.class);
 	}
 
 	@GET
@@ -56,7 +59,9 @@ public class CategoryController {
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getCategoryById(@PathParam("categoryId") Integer categoryId) {
-		return categoryService.getCategoryById(categoryId);
+		return client.target(BASE_URI + categoryId)
+				.request(MediaType.APPLICATION_JSON)
+				.get(Response.class);
 	}
 
 	@DELETE
@@ -64,6 +69,8 @@ public class CategoryController {
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response deleteById(@PathParam("categoryId") Integer categoryId) {
-		return categoryService.deleteCategory(categoryId);
+		return client.target(BASE_URI + categoryId)
+				.request(MediaType.APPLICATION_JSON)
+				.delete(Response.class);
 	}
 }
